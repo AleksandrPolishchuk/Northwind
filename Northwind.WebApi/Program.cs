9cfad08482d62e6a8a9c184b1be35e6bf3157c03
AP <aleksandrpolishchuk@gmail.com>
2023-04-04 23:33:48 +0700

    add proj-ref to Northwind webApi

--- Northwind.WebApi/Program.cs
+++ Northwind.WebApi/Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.AspNetCore.Mvc.Formatters; // IOutputFormatter, OutputFormatter
+using Packt.Shared; // AddNorthwindContext extension method
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddNorthwindContext();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
