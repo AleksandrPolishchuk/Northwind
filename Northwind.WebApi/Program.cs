4204c1b622a5ebc183098aae7eda73f60ee07580
AP <aleksandrpolishchuk@gmail.com>
2023-04-04 23:36:28 +0700

    add AddControllers lambda-block

--- Northwind.WebApi/Program.cs
+++ Northwind.WebApi/Program.cs
@@ -6,7 +6,28 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddNorthwindContext();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    WriteLine("Default output formatters:");
+    foreach (IOutputFormatter formatter in options.OutputFormatters)
+    {
+        OutputFormatter? mediaFormatter = formatter as OutputFormatter;
+        if (mediaFormatter is null)
+        {
+            WriteLine($"  {formatter.GetType().Name}");
+        }
+        else // OutputFormatter class has SupportedMediaTypes
+        {
+            WriteLine("  {0}, Media types: {1}",
+              arg0: mediaFormatter.GetType().Name,
+              arg1: string.Join(", ",
+                mediaFormatter.SupportedMediaTypes));
+        }
+    }
+})
+.AddXmlDataContractSerializerFormatters()
+.AddXmlSerializerFormatters();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
